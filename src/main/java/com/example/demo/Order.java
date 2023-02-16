596b2cf4d3a71ea6f894cc3dca2398de261a5342
Kaixuan_Xing <Kaixuan_Xing@atplatform.net>
2023-02-16 14:53:51 +0800

    Test JPA Pre

--- src/main/java/com/example/demo/Order.java
+++ src/main/java/com/example/demo/Order.java
@@ -3,34 +3,59 @@ package com.example.demo;
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
+import javax.persistence.*;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Data
+@Entity
+@Table(name = "Taco_Order")
 public class Order {
-    @NotBlank(message="Name is required")
-    private String name;
 
-    @NotBlank(message="Street is required")
-    private String street;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-    @NotBlank(message="City is required")
-    private String city;
+    private Date placedAt;
 
-    @NotBlank(message="State is required")
-    private String state;
+    @NotBlank(message = "Delivery name is required")
+    private String deliveryName;
 
-    @NotBlank(message="Zip code is required")
-    private String zip;
+    @NotBlank(message = "Street is required")
+    private String deliveryStreet;
 
-//    @CreditCardNumber(message="Not a valid credit card number")
+    @NotBlank(message = "City is required")
+    private String deliveryCity;
+
+    @NotBlank(message = "State is required")
+    private String deliveryState;
+
+    @NotBlank(message = "Zip code is required")
+    private String deliveryZip;
+
+//    @CreditCardNumber(message = "Not a valid credit card number")
     private String ccNumber;
 
-    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
-            message="Must be formatted MM/YY")
+    @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
+            message = "Must be formatted MM/YY")
     private String ccExpiration;
 
-    @Digits(integer=3, fraction=0, message="Invalid CVV")
+    @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
+
+    @ManyToMany(targetEntity = Taco.class)
+    private List<Taco> tacos = new ArrayList<>();
+
+    public void addDesign(Taco design) {
+        this.tacos.add(design);
+    }
+
+    @PrePersist
+    void placedAt() {
+        this.placedAt = new Date();
+    }
 }
--- src/main/java/com/example/demo/web/OrderController.java
+++ src/main/java/com/example/demo/web/OrderController.java
@@ -3,6 +3,7 @@ package com.example.demo.web;
 import javax.validation.Valid;
 
 import com.example.demo.Order;
+import com.example.demo.data.OrderRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -10,12 +11,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("order")
 public class OrderController {
+    private OrderRepository orderRepo;
+
+    public OrderController(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+    }
+
     @GetMapping("/current")
     public String orderForm(Model model) {
         model.addAttribute("order", new Order());
@@ -23,10 +33,13 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(@Valid Order order, Errors errors) {
+    public String processOrder(@Valid Order order, Errors errors,
+                               SessionStatus sessionStatus) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
+        orderRepo.save(order);
+        sessionStatus.setComplete();
         log.info("Order submitted: " + order);
         return "redirect:/";
     }
